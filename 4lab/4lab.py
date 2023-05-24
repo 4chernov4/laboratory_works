7d69d909f8c3108892dd077638bdf60d8aee0c28
4chernov4 <yarchernov@gmail.com>
2023-05-24 23:10:47 +0300

    Changes [3]

--- 4lab/4lab.py
+++ 4lab/4lab.py
@@ -1,38 +1,52 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-xcoordinate = float(input("Введите координату x: "))
-ycoordinate = float(input("Введите координату y: "))
-
-
-def f(x):
-    return x ** 2 + 2 * x - 10
+xi = float(input('Введите х:'))
+yi = float(input('Введите у:'))
 
 
-def df(x):
-    return -x ** 2 + 2 * x + 8
+def f1(x):
+    return x ** 2 + x - 14
 
 
-def gf(x):
-    return 2 * x + 4
+def f2(x):
+    return -x ** 2 + 2 * x + 10
 
 
-plt.axis([-10, 10, -100, 75])
-plt.title('3 графика и точка ', fontsize=20, fontname='Times New Roman')
-plt.xlabel('Ось х ', color='gray')
-plt.ylabel('Ось у ', color='gray')
-plt.grid(True)
-xvalue = np.linspace(-10, 10, 10)
-fig, ax = plt.subplots()
-plt.plot(xcoordinate, ycoordinate, 'ro')
-plt.plot(xvalue, f(xvalue), 'o-g', label='$x^2+2x-10$')
-plt.plot(xvalue, df(xvalue), 'o-b', label='$-x^2+2x+8$')
-plt.plot(xvalue, gf(xvalue), 'o-m', label='$2x+4$')
-plt.legend()
-ax.fill_between(xvalue, f(xvalue), df(xvalue), where=y2 > y1, interpolate=True,
+def f3(x):
+    return 2 * x - 6
+
+
+def check_point_in_area(x, y):
+    if f1(x) <= y <= f2(x) and y >= f3(x):
+        return True
+    else:
+        return False
+
+
+print(check_point_in_area(xi, yi))
+
+
+plt.axis([-10, 10, -20, 20])
+plt.title('Лаба 1', fontsize=20, fontname='Times New Roman')
+plt.xlabel('Ось x', color='gray')
+plt.ylabel('Ось y', color='gray')
+plt.ioff()
+x = np.arange(-10, 10, 0.01)
+y1 = x ** 2 + x - 14
+y2 = -x ** 2 + 2 * x + 10
+y3 = 2 * x - 6
+
+
+dn = np.maximum(y1, y3)
+
+plt.plot(x, y1, 'r', x, y2, 'b', x, y3, 'g', linestyle='solid')
+plt.fill_between(x, dn, y2, where=dn < y2, interpolate=True,
                 color='green', alpha=0.3)
-ax.fill_between(x, y2, y3, where=y2 > y3, interpolate=True,
-                color='yellow', alpha=0.2)
-ax.fill_between(x, y3, y1, where=y3 > y1, interpolate=True,
-                color='blue', alpha=0.2)
+
+plt.plot(xi, yi, "r*")
+plt.text(xi, yi, check_point_in_area(xi, yi), fontsize=10, bbox={'facecolor': 'yellow', 'alpha': 0.2})
+lgnd = plt.legend([r'$y = x^2+2x-14$', r'$y = -x^2+2x+8$', r'$y = 2x+4$'], loc=2, shadow=True)
+lgnd.get_frame().set_facecolor('#ffb19a')
+
 plt.show()
